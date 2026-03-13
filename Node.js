b70334844e5333f27dae4b43d02cca3993531423
Tom Sharp <tomsharp924@gmail.com>
2026-03-13 14:10:37 -0700

    Slight clean up

--- Node.js
+++
@@ -1,7 +0,0 @@
-app.use(express.static('public', {
-    setHeaders: (res, path) => {
-      if (path.endsWith('.wasm')) {
-        res.setHeader('Content-Type', 'application/wasm');
-      }
-    }
-  }));
